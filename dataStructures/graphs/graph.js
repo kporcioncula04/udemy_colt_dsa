1b16b178fb7ea2bd4e84fb3facd20c3904d5c0e5
Karolina <kporcioncula.04@gmail.com>
2020-12-18 15:54:08 -0500

    finished: graph ds

--- dataStructures/graphs/graph.js
+++ dataStructures/graphs/graph.js
@@ -8,9 +8,43 @@ class Graph{
   addVertex(vertex){
     if(!this.adjacenyList[vertex]) this.adjacenyList[vertex] = []
   }
+
+  addEdge(vertex1, vertex2){
+    this.adjacenyList[vertex1].push(vertex2)
+    this.adjacenyList[vertex2].push(vertex1)
+  }
+
+  removeEdge(vertex1, vertex2){
+    this.adjacenyList[vertex1] = this.adjacenyList[vertex1].filter(
+      v => v !== vertex2
+    )
+
+    this.adjacenyList[vertex2] = this.adjacenyList[vertex1].filter(
+      v => v !== vertex2
+    )
+  }
+
+  removeVertex(vertex){
+    while(this.adjacenyList[vertex].length){
+      const adjacentVertex = this.adjacenyList[vertex].pop()
+      this.removeEdge(vertex, adjacentVertex)
+    }
+    delete this.adjacenyList[vertex]
+  }
 }
 
 let graph = new Graph()
-graph.addVertex('hello')
-graph.addVertex('hi')
+graph.addVertex('Tokyo')
+graph.addVertex('Dallas')
+graph.addVertex('Aspen')
+graph.addVertex('Los Angeles')
+graph.addVertex('Hong Kong')
+graph.addEdge('Dallas', 'Tokyo')
+graph.addEdge('Dallas', 'Aspen')
+graph.addEdge('Hong Kong', 'Tokyo')
+graph.addEdge('Hong Kong', 'Dallas')
+graph.addEdge('Los Angeles', 'Hong Kong')
+graph.addEdge('Los Angeles', 'Aspen')
+graph.removeVertex('Hong Kong')
+
 console.log(graph)
