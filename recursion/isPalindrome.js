e5f7436c1aa700ad65ca9e9ff37b34394e24bcba
Karolina <kporcioncula.04@gmail.com>
2021-01-12 16:16:16 -0500

    update: isPalindrome prob

--- recursion/isPalindrome.js
+++ recursion/isPalindrome.js
@@ -1,4 +1,7 @@
 function isPalindrome(str){
+
+    str = str.replace(/[^0-9a-zA-Z]+/gmi,"");
+    str = str.toLowerCase();
   if(str.length === 1) return true
   if(str.length === 2) return str[0] === str[1]
   if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
@@ -6,3 +9,7 @@ function isPalindrome(str){
 }
 
 console.log(isPalindrome('awesome'))
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('kayak'))
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+
